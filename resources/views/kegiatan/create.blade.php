3772dff95f23bc562083fd94d4518fed2490a312
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-08-09 17:35:36 +0700

    ubah layout, nambah anggota kegiatan pakai tombol untuk nampilkan listbox yang isinya nama user

--- resources/views/kegiatan/create.blade.php
+++ resources/views/kegiatan/create.blade.php
@@ -8,13 +8,10 @@
     <div class="page-header">
         <h1>Buat Kegiatan Baru</h1>
     </div>
+    <button class="btn btn-default pull-right" id="tombol">Tambah Anggota Kegiatan</button><br>
+
     {{ Form::open(['route' => 'kegiatan.store']) }}
     <div class="col-lg-6">
-        <div class="form-group">
-            {{ Form::label('kode_proyek', 'ID Kegiatan', ['class' => 'control-label']) }}
-            {{ Form::text('kode_proyek', null, ['class' => 'form-control']) }}
-        </div>
-
         <div class="form-group">
             {{ Form::label('nama_proyek', 'Nama Kegiatan', ['class' => 'control-label']) }}
             {{ Form::text('nama_proyek', null, ['class' => 'form-control']) }}
@@ -36,24 +33,31 @@
         </div>
     </div>
     <div class="col-lg-6">
-
-        {{ Form::label(null, 'Anggota:', ['class' => 'control-label']) }}
-        <div class=scrollable>
-            <table class="table">
-                @foreach($users as $user)
-                    <tr>
-                        <td>{{ Form::checkbox('anggota[]', $user->id) }}</td>
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->name }}</td>
-                    </tr>
-                @endforeach
-            </table>
+        <div class="form-group" id="list_anggota">
+            {{ Form::label(null, 'Anggota:', ['class' => 'control-label']) }}
+            <br>
         </div>
 
         <br>
-        {{ Form::submit('Daftarkan Kegiatan', ['class' => 'btn btn-primary']) }}
+        {{ Form::submit('Daftarkan Kegiatan', ['class' => 'btn btn-primary pull-right']) }}
         {{ Form::close() }}
-
     </div>
 
 @endsection
+
+@section('js')
+    <script>
+        $(function () {
+            $('#tombol').on('click', function () {
+                $('<label for="nama">Nama</label>\n' +
+                    '        <select class="form-control" name="nama[]" id="nama" data-parsley-required="true">\n' +
+                    '          @foreach ($users as $user) \n' +
+                    '          {\n' +
+                    '            <option value="{{ $user->id }}" id="nama[]">{{ $user->name }}</option>\n' +
+                    '          }\n' +
+                    '          @endforeach\n' +
+                    '        </select><br>\n').appendTo('#list_anggota');
+            });
+        });
+    </script>
+    @endsection
