15a1f29b28cf1fe1526a45f8a232cf59eee33bbf
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-08-09 07:57:40 +0700

    layout ikuti halaman welcome

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,133 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+    <title>Portal Divisi Satelit Jaringan Komunikasi</title>
 
-                        <div class="form-group{{ $errors->has('id') ? ' has-error' : '' }}">
-                            <label for="id" class="col-md-4 control-label">ID</label>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-                            <div class="col-md-6">
-                                <input id="id" type="text" class="form-control" name="id" value="{{ old('id') }}" required autofocus>
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
 
-                                @if ($errors->has('id'))
-                                    <span class="help-block">
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 40px;
+            text-align: center;
+        }
+
+        .links > a {
+            /*color: #636b6f;*/
+            /*padding: 0 25px;*/
+            /*font-size: 12px;*/
+            /*font-weight: 600;*/
+            /*letter-spacing: .1rem;*/
+            /*text-decoration: none;*/
+            /*text-transform: uppercase;*/
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @if (Auth::check())
+                <a href="{{ url('/home') }}">Home</a>
+            @endif
+        </div>
+    @endif
+
+    <div class="content">
+        <img src="{{ asset('bootstrap/img/bri-sat.jpg') }}" style="display: inline-block; " href="{{ route('home') }}">
+        <div class="title">
+            Portal Divisi Satelit Jaringan Komunikasi
+            <br><br>
+        </div>
+
+        <div class="links">
+            <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
+
+                <div class="form-group{{ $errors->has('id') ? ' has-error' : '' }}">
+                    <label for="id" class="col-md-4 control-label">ID</label>
+
+                    <div class="col-md-6">
+                        <input id="id" type="text" class="form-control" name="id" value="{{ old('id') }}" required autofocus>
+
+                        @if ($errors->has('id'))
+                            <span class="help-block">
                                         <strong>{{ $errors->first('id') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+                        @endif
+                    </div>
+                </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                    <div class="col-md-6">
+                        <input id="password" type="password" class="form-control" name="password" required>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
+                        @if ($errors->has('password'))
+                            <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                        @endif
+                    </div>
                 </div>
-            </div>
+
+                <div class="form-group">
+                    <div class="col-md-8 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary btn-lg">
+                            <span class="glyphicon glyphicon-log-in"></span> Login
+                        </button>
+                        {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
+                        {{--Forgot Your Password?--}}
+                        {{--</a>--}}
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
-@endsection
+</body>
+</html>
