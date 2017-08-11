f1abe8cfca668a013c2c7fc87da19f19eeb6bc4b
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-08-11 11:03:38 +0700

    tambah library jscroll

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -122,6 +122,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/bootbox.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.jscroll.js') }}"></script>
     @yield('js')
 </body>
 </html>
