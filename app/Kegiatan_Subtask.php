175a3b3e180223d7fdff60b24f63526db2f70663
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-08-11 11:06:36 +0700

    ganti nama dari proyek_tugas

---
+++ app/Kegiatan_Subtask.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kegiatan_Subtask extends Model
+{
+    protected $table = 'kegiatan_subtask';
+
+    protected $fillable = [
+        'id', 'kode_kegiatan', 'id_pembuat', 'nama_tugas', 'id_pegawai_mengerjakan', 'status', 'updated_at'
+    ];
+}
